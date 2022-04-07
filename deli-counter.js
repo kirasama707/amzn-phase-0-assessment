8accf8f604b926fad09b8c744266e8e7b9a036f0
christian <vagetto707@aol.com>
2022-04-07 16:56:27 -0700

    completed assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,4 +1,5 @@
 // 1. Write your functions here
+const katzDeli = []
 
 
 // 2. Example Usage
@@ -22,3 +23,61 @@
 // nowServing(katzDeli) //=> "Currently serving Grace."
 
 // line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+
+
+//need to make an for loops cause we need to iterate through the array to different customers.
+//need to make an .push so we can add customers to an array.
+// need to use .shift cause it removes the first element unlike pop which only removes the last element.
+
+//console.log(nameOfCustomer = "Ada", "Grace", "Kent")
+
+function takeANumber(katzLineQue, nameOfCustomer) {
+    katzLineQue.push(nameOfCustomer);
+    for (let [index, value] of katzDeli.entries()){
+        if (value === nameOfCustomer)
+        console.log(`welcome, ${value}. you are currently number ${index +=1} in line.`)
+    }
+
+}
+
+
+//input array
+//output current place in line
+//side effects
+
+//step one- need to use the join method.
+let lineQue = []
+
+function line (katzLineQue) {
+    for(let i = 0; i < katzLineQue.length; i ++) {
+        let newCustomer = katzLineQue[i];
+        lineQue.push(' ' + [i + 1] + '. ' + newCustomer)
+        
+    }
+    if(katzLineQue === 0) {
+        `the line is currently empty`
+    }
+    else lineUpdate = lineQue.splice(0,3)
+    return "the line is currently" + `${lineUpdate.join(" ")}`
+
+}
+
+   function nowServing(katzLineQue) {
+    if(katzLineQue.length > []) {
+        return console.log(`"currently serving ${katzLineQue.shift()}"`);
+    } else {
+        return "there is nobody waiting to be served!";
+    }
+}
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+console.log(line(katzDeli))
+nowServing(katzDeli)
+console.log(line(katzDeli))
+takeANumber(katzDeli, "Matz")
+console.log(line(katzDeli))
+nowServing(katzDeli)
+console.log(line(katzDeli))
+
+ 
